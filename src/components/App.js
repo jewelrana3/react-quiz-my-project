85ef86c8a726234636047d96ad861db79aa98b33
Jewel Rana <juyelrana7752@gmail.com>
2024-03-14 16:17:12 +0600

    react quiz app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+}
---
+++ src/components/Videos.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "../styles/videos.module.css";
+import Video from "./Video";
+export default function Videos() {
+  return (
+    <div className={classes.videos}>
+      <Video />
+    </div>
+  );
+}
